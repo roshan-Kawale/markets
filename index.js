f61821a0d35b035a35867341c60ef8d2135fada8
Roshan <roshankawale43@gmail.com>
2024-07-07 14:30:18 +0530

    adding the cors

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: 'https://markets-two.vercel.app', // or ''
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-}));
-
+// app.use(cors({
+//   exposedHeaders: ['X-Total-Count']
+// }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 dotenv.config(); 
 // Connect to MongoDB using Mongoose
