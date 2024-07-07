ff18069550ff06cb725b7a15b889503528247b32
Roshan <roshankawale43@gmail.com>
2024-07-07 17:11:39 +0530

    updation

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({
-//   exposedHeaders: ['X-Total-Count']
-// }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS');
